330ce7b08ee7c5dfd6370bf4caf19523b07421ed
Siddhant Arya <siddhant.arya18@gmail.com>
2018-05-22 16:51:07 +0530

    Adding support for DAGs

--- Java/pkg/graphs/Graph.java
+++ Java/pkg/graphs/Graph.java
@@ -6,25 +6,43 @@ import java.util.ArrayList;
 public class Graph
 {
   int vertices;
+  boolean directional;
   ArrayList<LinkedList<Integer>> adjList;
 
   public Graph()
   {
-    this(5);
+    this(5, false);
+  }
+
+  public Graph(boolean x)
+  {
+    this(5, x);
   }
 
   public Graph(int n)
   {
+    this(n, false);
+  }
+
+  public Graph(int n, boolean dir)
+  {
     vertices = n;
+    directional = dir;
     adjList = new ArrayList<LinkedList<Integer>>(n);
     for (int i = 0; i<vertices; i++)
       adjList.add(new LinkedList<Integer>());
   }
 
+
   public void addEdge(int src, int dest)
   {
-    adjList.get(src).add(dest);
-    adjList.get(dest).add(src);
+    if (directional == false)
+    {
+      adjList.get(src).add(dest);
+      adjList.get(dest).add(src);
+    }
+    else
+      adjList.get(src).add(dest);
   }
 
   public int getVertices()
